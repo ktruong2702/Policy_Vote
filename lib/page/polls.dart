bf9ca63de8cafe0d356d0a6d2c6d3db20f35538d
MasonZ20 <88959698+MasonZ20@users.noreply.github.com>
2024-04-05 10:18:43 +0700

    Organize the layout

--- lib/page/polls.dart
+++ lib/page/polls.dart
@@ -1,176 +1,155 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import 'package:bai3/services/api_service.dart';
-
-class PollPage extends StatefulWidget {
-  const PollPage({super.key});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _QuizSelectionPageState createState() => _QuizSelectionPageState();
-}
-
-class _QuizSelectionPageState extends State<PollPage> {
-  String? selectedQuestion;
-  List<dynamic> _questions = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchQuestions();
-  }
-
-  Future<void> _fetchQuestions() async {
-    try {
-      List<dynamic> questions = await fetchQuestions();
-      setState(() {
-        _questions = questions;
-      });
-    } catch (e) {
-      // Handle the error
-      print('Error: $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          const SliverAppBar(
-            title: Text('Home'),
-            centerTitle: true,
-            automaticallyImplyLeading: false,
-            floating:
-                true, // Đặt floating thành true để SliverAppBar có thể đặt cố định khi cuộn
-            pinned:
-                true, // Đặt pinned thành true để SliverAppBar luôn hiển thị khi cuộn lên đầu
-            backgroundColor: Colors.blue,
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.all(10.0),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  final question = _questions[index];
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                      side: const BorderSide(
-                        color: Colors.black,
-                        width: 3.0,
-                      ),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: ListTile(
-                      leading: const FlutterLogo(size: 60.0),
-                      title: Text(
-                        question['text'],
-                        style: TextStyle(
-                          color: selectedQuestion == question['id'].toString()
-                              ? Colors.black
-                              : Colors.grey,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      onTap: () {
-                        setState(() {
-                          selectedQuestion = question['id'].toString();
-                        });
-                      },
-                      selected: selectedQuestion == question['id'].toString(),
-                      subtitle: const Text(
-                        'A sufficiently long subtitle warrants three lines.',
-                      ),
-                      trailing: const Icon(Icons.more_vert),
-                      isThreeLine: true,
-                      tileColor: const Color.fromARGB(255, 252, 251, 251),
-                      textColor: const Color.fromARGB(236, 0, 0, 0),
-                    ),
-                  );
-                },
-                childCount: _questions.length,
-              ),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (selectedQuestion != null) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => QuizPage(selectedQuestion!)),
-            );
-          } else {
-            // Hiển thị thông báo nếu người dùng chưa chọn câu hỏi
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('Please choose a poll to vote'),
-            ));
-          }
-        },
-        child: const Icon(Icons.arrow_forward),
-      ),
-    );
-  }
-}
-
-class QuizPage extends StatefulWidget {
-  final String selectedQuestion;
-
-  const QuizPage(this.selectedQuestion, {Key? key}) : super(key: key);
-
-  @override
-  _QuizPageState createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
-  List<String> _selectedOptions = [];
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.selectedQuestion),
+        automaticallyImplyLeading: false, // Ẩn nút quay về
+        centerTitle: true, // Căn giữa tiêu đề
+        title: const Text(
+          'Home',
+          style: TextStyle(
+            color: Colors.white, // Màu chữ
+            fontSize: 20.0, // Cỡ chữ
+            fontWeight: FontWeight.bold, // Độ đậm
+          ),
+        ),
+        backgroundColor: Colors.blueAccent, // Màu nền
+        elevation: 0, // Bỏ bóng đổ
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('You choose the question is: ${widget.selectedQuestion}'),
-            const SizedBox(height: 20),
-            Column(
-              children: _buildOptions(),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Bắt đầu làm bài thi với câu hỏi đã chọn và các câu trả lời được chọn
-              },
-              child: const Text('Vote'),
-            ),
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color.fromARGB(255, 48, 173, 100), Colors.blue],
+          ),
+        ),
+        child: Center(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('polls')
+                .orderBy('title')
+                .snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              return ListView(
+                padding: const EdgeInsets.all(8.0),
+                children:
+                    snapshot.data!.docs.map((DocumentSnapshot pollDocument) {
+                  Map<String, dynamic> pollData =
+                      pollDocument.data() as Map<String, dynamic>;
+                  String? pollId =
+                      pollDocument.id; // Assuming poll ID is needed
+
+                  return Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        pollData['title'],
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        pollData['description'],
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                QuestionListPage(pollId: pollId),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }).toList(),
+              );
+            },
+          ),
         ),
       ),
     );
   }
+}
 
-  List<Widget> _buildOptions() {
-    return [
-      for (int i = 0; i < 4; i++)
-        CheckboxListTile(
-          title: Text('The answer ${i + 1}'),
-          value: _selectedOptions.contains('The answer ${i + 1}'),
-          onChanged: (value) {
-            setState(() {
-              if (value!) {
-                _selectedOptions.add('The answer ${i + 1}');
-              } else {
-                _selectedOptions.remove('The answer ${i + 1}');
-              }
-            });
-          },
-        ),
-    ];
+
+
+class QuestionListPage extends StatelessWidget {
+  final String? pollId;
+
+  const QuestionListPage({Key? key, this.pollId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Questions for Poll'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('questions')
+            .where('poll_id', isEqualTo: pollId)
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot questionDocument = snapshot.data!.docs[index];
+              return FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('options')
+                    .where('question_id', isEqualTo: questionDocument.id)
+                    .get(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> optionSnapshot) {
+                  if (optionSnapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+                  if (optionSnapshot.hasError) {
+                    return Text('Error: ${optionSnapshot.error}');
+                  }
+                  return ListTile(
+                    title: Text(questionDocument['question_txt']),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: optionSnapshot.data!.docs.map((DocumentSnapshot optionDocument) {
+                        return RadioListTile<String>(
+                          title: Text(optionDocument['option_txt']),
+                          value: optionDocument.id,
+                          groupValue: null, // Điền giá trị của nhóm ở đây nếu bạn muốn sử dụng RadioButton
+                          onChanged: (String? value) {
+                            // Viết logic của bạn ở đây để xử lý khi người dùng chọn một câu trả lời
+                            // Ví dụ: update dữ liệu trong Firebase
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }

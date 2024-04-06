93eb18fbaa6c142ff09e1e09726c5279505135fc
razsa <21493e@gmail.com>
2024-04-06 17:02:41 -0400

    feat: Refactored code()

--- lib/page/polls.dart
+++ lib/page/polls.dart
@@ -33,11 +33,8 @@ class _HomePageState extends State<HomePage> {
         color: Colors.white,
         child: Center(
           child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('polls')
-                .orderBy('title')
-                .snapshots(),
-            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            stream: PollService().getPolls(),
+            builder: (context, AsyncSnapshot<List<Poll>> snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const CircularProgressIndicator();
               }
@@ -45,49 +42,17 @@ class _HomePageState extends State<HomePage> {
                 return Text('Error: ${snapshot.error}');
               }
 
-              List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+              List<Poll> polls = snapshot.data!;
 
-              List<QueryDocumentSnapshot> visibleQuestions =
-                  showAllQuestions ? documents : documents.take(5).toList();
+              List<Poll> visiblePolls =
+                  showAllQuestions ? polls : polls.take(5).toList();
 
-              return ListView(
+              return ListView.builder(
                 padding: const EdgeInsets.all(8.0),
-                children: [
-                  ...visibleQuestions.map((DocumentSnapshot pollDocument) {
-                    Map<String, dynamic> pollData =
-                        pollDocument.data() as Map<String, dynamic>;
-                    String? pollId = pollDocument.id;
-
-                    return Card(
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: ListTile(
-                        title: Text(
-                          pollData['title'],
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          pollData['description'],
-                          style: const TextStyle(fontStyle: FontStyle.italic),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  QuestionListPage(pollId: pollId),
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  }),
-                  if (!showAllQuestions &&
-                      documents.length > 5 &&
-                      showLimitedQuestions)
-                    TextButton(
+                itemCount: visiblePolls.length + (showLimitedQuestions ? 1 : 0),
+                itemBuilder: (context, index) {
+                  if (index == visiblePolls.length && showLimitedQuestions) {
+                    return TextButton(
                       onPressed: () {
                         setState(() {
                           showAllQuestions = true;
@@ -95,18 +60,36 @@ class _HomePageState extends State<HomePage> {
                         });
                       },
                       child: Text('See All'),
-                    ),
-                  if (showAllQuestions)
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          showAllQuestions = false;
-                          showLimitedQuestions = true;
-                        });
-                      },
-                      child: Text('Show Less'),
-                    ),
-                ],
+                    );
+                  } else {
+                    Poll poll = visiblePolls[index];
+                    return Card(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: ListTile(
+                        title: Text(
+                          poll.title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          poll.description,
+                          style: const TextStyle(fontStyle: FontStyle.italic),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  QuestionListPage(pollId: poll.id),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  }
+                },
               );
             },
           ),
@@ -116,10 +99,23 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class QuestionListPage extends StatelessWidget {
+class QuestionListPage extends StatefulWidget {
   final String? pollId;
-
   const QuestionListPage({Key? key, this.pollId}) : super(key: key);
+
+  @override
+  _QuestionListPageState createState() => _QuestionListPageState();
+}
+
+class _QuestionListPageState extends State<QuestionListPage> {
+  Map<String, String> _userAnswers = {};
+
+  void _handleAnswerSelected(String questionId, String answerId) {
+    setState(() {
+      _userAnswers[questionId] = answerId;
+    });
+    UserAnswerService().saveUserAnswer(questionId, answerId);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -127,29 +123,23 @@ class QuestionListPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Questions for Poll'),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('questions')
-            .where('poll_id', isEqualTo: pollId)
-            .snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+      body: FutureBuilder(
+        future: QuestionService().getQuestionsForPoll(widget.pollId!),
+        builder: (context, AsyncSnapshot<List<Question>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return CircularProgressIndicator();
           }
           if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           }
+          List<Question> questions = snapshot.data!;
           return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
+            itemCount: questions.length,
             itemBuilder: (context, index) {
-              DocumentSnapshot questionDocument = snapshot.data!.docs[index];
+              Question question = questions[index];
               return FutureBuilder(
-                future: FirebaseFirestore.instance
-                    .collection('options')
-                    .where('question_id', isEqualTo: questionDocument.id)
-                    .get(),
-                builder:
-                    (context, AsyncSnapshot<QuerySnapshot> optionSnapshot) {
+                future: OptionService().getOptionsForQuestion(question.id),
+                builder: (context, AsyncSnapshot<List<Option>> optionSnapshot) {
                   if (optionSnapshot.connectionState ==
                       ConnectionState.waiting) {
                     return CircularProgressIndicator();
@@ -157,24 +147,40 @@ class QuestionListPage extends StatelessWidget {
                   if (optionSnapshot.hasError) {
                     return Text('Error: ${optionSnapshot.error}');
                   }
-                  return ListTile(
-                    title: Text(questionDocument['question_txt']),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: optionSnapshot.data!.docs
-                          .map((DocumentSnapshot optionDocument) {
-                        return RadioListTile<String>(
-                          title: Text(optionDocument['option_txt']),
-                          value: optionDocument.id,
-                          groupValue:
-                              null, // Điền giá trị của nhóm ở đây nếu bạn muốn sử dụng RadioButton
-                          onChanged: (String? value) {
-                            // Viết logic của bạn ở đây để xử lý khi người dùng chọn một câu trả lời
-                            // Ví dụ: update dữ liệu trong Firebase
-                          },
-                        );
-                      }).toList(),
-                    ),
+                  List<Option> options = optionSnapshot.data!;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        title: Text(question.text),
+                      ),
+                      FutureBuilder(
+                        future: AnswerService().getAnswersForQuestion(question.id),
+                        builder: (context, AsyncSnapshot<List<Answer>> answerSnapshot) {
+                          if (answerSnapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return CircularProgressIndicator();
+                          }
+                          if (answerSnapshot.hasError) {
+                            return Text('Error: ${answerSnapshot.error}');
+                          }
+                          List<Answer> answers = answerSnapshot.data!;
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: answers.map((answer) {
+                              return RadioListTile<String>(
+                                title: Text(answer.text),
+                                value: answer.id,
+                                groupValue: _userAnswers[question.id],
+                                onChanged: (String? value) {
+                                  _handleAnswerSelected(question.id, answer.id);
+                                },
+                              );
+                            }).toList(),
+                          );
+                        },
+                      ),
+                    ],
                   );
                 },
               );
@@ -182,6 +188,221 @@ class QuestionListPage extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+/// Service class for saving user answers to questions in Firestore.
+///
+/// This class provides a method to save user answers to the 'user_answers'
+/// collection in Firestore. It encapsulates the Firestore-specific logic
+/// and allows other parts of the application to easily save user answers.
+///
+/// Usage:
+///   1. Create an instance of `UserAnswerService`.
+///   2. Call the `saveUserAnswer` method with the `questionId` and `answerId`
+///      whenever a user selects an answer to a question.
+///
+/// Example:
+///   final userAnswerService = UserAnswerService();
+///   userAnswerService.saveUserAnswer('question1', 'answer2');
+class UserAnswerService {
+  final CollectionReference _userAnswersCollection =
+      FirebaseFirestore.instance.collection('user_answers');
+
+      /// Saves a user's answer to a question in Firestore.
+  ///
+  /// [questionId] is the ID of the question.
+  /// [answerId] is the ID of the selected answer.
+  ///
+  /// Returns a [Future] that completes when the answer is saved.
+  Future<void> saveUserAnswer(String questionId, String answerId) async {
+    await _userAnswersCollection.doc().set({
+      'question_id': questionId,
+      'answer_id': answerId,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+  }
+}
+
+/// Service class for retrieving polls from Firestore.
+///
+/// This class provides a method to retrieve a stream of polls from the 'polls'
+/// collection in Firestore. It encapsulates the Firestore-specific logic
+/// and allows other parts of the application to easily access the polls data.
+///
+/// Usage:
+///   1. Create an instance of `PollService`.
+///   2. Call the `getPolls` method to retrieve a stream of polls.
+///   3. Listen to the stream and handle the emitted list of `Poll` objects.
+///
+/// Example:
+///   final pollService = PollService();
+///   final pollStream = pollService.getPolls();
+///   pollStream.listen((polls) {
+///     // Handle the list of polls
+///   });
+class PollService {
+  final CollectionReference _pollsCollection =
+      FirebaseFirestore.instance.collection('polls');
+
+ /// Retrieves a stream of polls from Firestore.
+  ///
+  /// The polls are ordered by the 'title' field in ascending order.
+  ///
+  /// Returns a [Stream] that emits a list of [Poll] objects.
+  Stream<List<Poll>> getPolls() {
+    return _pollsCollection.orderBy('title').snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => Poll.fromJson(doc.data() as Map<String, dynamic>, doc.id)).toList());
+
+  }
+}
+
+/// Service class for retrieving questions for a specific poll from Firestore.
+///
+/// This class provides a method to retrieve a list of questions associated with
+/// a specific poll ID from the 'questions' collection in Firestore.
+class QuestionService {
+  final CollectionReference _questionsCollection =
+      FirebaseFirestore.instance.collection('questions');
+
+  /// Retrieves a list of questions for a specific poll ID from Firestore.
+  ///
+  /// [pollId] is the ID of the poll.
+  ///
+  /// Returns a [Future] that completes with a list of [Question] objects.
+  Future<List<Question>> getQuestionsForPoll(String pollId) async {
+    QuerySnapshot snapshot = await _questionsCollection
+        .where('poll_id', isEqualTo: pollId)
+        .get();
+    return snapshot.docs.map((doc) => Question.fromJson(doc.data() as Map<String, dynamic>, doc.id)).toList();
+  }
+}
+
+/// Service class for retrieving options for a specific question from Firestore.
+///
+/// This class provides a method to retrieve a list of options associated with
+/// a specific question ID from the 'options' collection in Firestore.
+class OptionService {
+  final CollectionReference _optionsCollection =
+      FirebaseFirestore.instance.collection('options');
+
+  /// Retrieves a list of options for a specific question ID from Firestore.
+  ///
+  /// [questionId] is the ID of the question.
+  ///
+  /// Returns a [Future] that completes with a list of [Option] objects.
+  Future<List<Option>> getOptionsForQuestion(String questionId) async {
+    QuerySnapshot snapshot = await _optionsCollection
+        .where('question_id', isEqualTo: questionId)
+        .get();
+    return snapshot.docs.map((doc) => Option.fromJson(doc.data() as Map<String, dynamic>, doc.id)).toList();
+  }
+}
+
+/// Service class for retrieving answers for a specific question from Firestore.
+///
+/// This class provides a method to retrieve a list of answers associated with
+/// a specific question ID from the 'answers' collection in Firestore.
+class AnswerService {
+  final CollectionReference _answersCollection =
+      FirebaseFirestore.instance.collection('answers');
+
+  /// Retrieves a list of answers for a specific question ID from Firestore.
+  ///
+  /// [questionId] is the ID of the question.
+  ///
+  /// Returns a [Future] that completes with a list of [Answer] objects.
+  Future<List<Answer>> getAnswersForQuestion(String questionId) async {
+    QuerySnapshot snapshot = await _answersCollection
+        .where('question_id', isEqualTo: questionId)
+        .get();
+    return snapshot.docs.map((doc) => Answer.fromJson(doc.data() as Map<String, dynamic>, doc.id)).toList();
+  }
+}
+
+/// Represents a poll.
+class Poll {
+  final String id;
+  final String title;
+  final String description;
+
+  Poll({required this.id, required this.title, required this.description});
+
+  /// Creates a [Poll] object from a JSON map and an ID.
+  ///
+  /// [json] is the JSON map containing the poll data.
+  /// [id] is the ID of the poll.
+  ///
+  /// Returns a new [Poll] object.
+  factory Poll.fromJson(Map<String, dynamic> json, String id) {
+    return Poll(
+      id: id,
+      title: json['title'],
+      description: json['description'],
+    );
+  }
+}
+
+/// Represents a question.
+class Question {
+  final String id;
+  final String text;
+
+  Question({required this.id, required this.text});
+
+  /// Creates a [Question] object from a JSON map and an ID.
+  ///
+  /// [json] is the JSON map containing the question data.
+  /// [id] is the ID of the question.
+  ///
+  /// Returns a new [Question] object.
+  factory Question.fromJson(Map<String, dynamic> json, String id) {
+    return Question(
+      id: id,
+      text: json['question_txt'],
+    );
+  }
+}
+
+/// Represents an option for a question.
+class Option {
+  final String id;
+  final String text;
+
+  Option({required this.id, required this.text});
+
+  /// Creates an [Option] object from a JSON map and an ID.
+  ///
+  /// [json] is the JSON map containing the option data.
+  /// [id] is the ID of the option.
+  ///
+  /// Returns a new [Option] object.
+  factory Option.fromJson(Map<String, dynamic> json, String id) {
+    return Option(
+      id: id,
+      text: json['option_txt'],
+    );
+  }
+}
+
+/// Represents an answer to a question.
+class Answer {
+  final String id;
+  final String text;
+
+  Answer({required this.id, required this.text});
+
+  /// Creates an [Answer] object from a JSON map and an ID.
+  ///
+  /// [json] is the JSON map containing the answer data.
+  /// [id] is the ID of the answer.
+  ///
+  /// Returns a new [Answer] object.
+  factory Answer.fromJson(Map<String, dynamic> json, String id) {
+    return Answer(
+      id: id,
+      text: json['answer_txt'],
     );
   }
 }

0748a946d0d415aa2e1fd8aed7a8101bab1c180d
Khang Q Truong <kingtruong2702@gmail.com>
2024-04-06 23:43:50 -0400

    implemented login for real this time

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,8 +1,12 @@
 import 'package:bai3/mainpage.dart';
+import 'package:bai3/model/my_user.dart';
 import 'package:bai3/page/splashscreen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:bai3/page/polls.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class LoginForm extends StatefulWidget {
   const LoginForm({Key? key}) : super(key: key);
@@ -16,24 +20,56 @@ class _LoginFormState extends State<LoginForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void _login() {
+// void _login() {
+  //   String email = _emailController.text;
+  //   String password = _passwordController.text;
+  //   String username = _usernameController.text;
+
+  //   // Mock login logic
+  //   if (email == 'user@example.com' || password == 'password' && username == 'testusername' || password == 'password') {
+  //     // Successful login
+  //     Navigator.pushReplacement(
+  //       context,
+  //       MaterialPageRoute(builder: (context) => const Mainpage()),
+  //     );
+  //   } else {
+  //     // Invalid credentials
+  //     showDialog(
+  //       context: context,
+  //       builder: (context) => AlertDialog(
+  //         title: const Text('Invalid Credentials'),
+  //         content: const Text('Please enter valid email/username and password.'),
+  //         actions: [
+  //           TextButton(
+  //             onPressed: () => Navigator.pop(context),
+  //             child: const Text('OK'),
+  //           ),
+  //         ],
+  //       ),
+  //     );
+  //   }
+  // }
+
+
+  void _login() async {
     String email = _emailController.text;
     String password = _passwordController.text;
-
-    // Mock login logic
-    if (email == 'user@example.com' && password == 'password') {
-      // Successful login
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const Mainpage()),
+        MaterialPageRoute(builder: (context) => const Mainpage())
       );
-    } else {
-      // Invalid credentials
+    } catch (e) {
+      // If login fails, show an error dialog
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
-          title: const Text('Invalid Credentials'),
-          content: const Text('Please enter valid email and password.'),
+          title: const Text('Login Failed'),
+          content: const Text('Your email or password is incorrect. Please try again.'),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
@@ -48,14 +84,15 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back), // Icon cho nút back
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SplashScreen()),
-              );
+              context,
+              MaterialPageRoute(builder: (context) => const SplashScreen()),
+            );
           },
         ),
       ),
@@ -77,7 +114,7 @@ class _LoginFormState extends State<LoginForm> {
               decoration: const InputDecoration(
                 labelText: 'Email',
                 labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold, // Thiết lập độ đậm cho chữ label
+                  fontWeight: FontWeight.bold,
                 ),
                 enabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(
@@ -86,7 +123,7 @@ class _LoginFormState extends State<LoginForm> {
                   ),
                   borderRadius: BorderRadius.all(
                     Radius.circular(20.0),
-                  ), // Độ cong của góc viền
+                  ),
                   gapPadding: 150.0,
                 ),
               ),
@@ -97,7 +134,7 @@ class _LoginFormState extends State<LoginForm> {
               decoration: const InputDecoration(
                 labelText: 'Password',
                 labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold, // Thiết lập độ đậm cho chữ label
+                  fontWeight: FontWeight.bold,
                 ),
                 enabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(
@@ -106,15 +143,14 @@ class _LoginFormState extends State<LoginForm> {
                   ),
                   borderRadius: BorderRadius.all(
                     Radius.circular(20.0),
-                  ), // Độ cong của góc viền
+                  ),
                   gapPadding: 150.0,
                 ),
               ),
-              obscureText: true, //Hide text password
+              obscureText: true,
             ),
             const SizedBox(height: 20.0),
             ElevatedButton(
-              // Modify the onPressed callback
               onPressed: _login,
               child: const Text('Login'),
             ),
@@ -124,6 +160,3 @@ class _LoginFormState extends State<LoginForm> {
     );
   }
 }
-
-
-
